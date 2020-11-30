60915ca7a3338cd61a7bf799bd16bd4af96dd5e1
sunxiaoyu <sunxiaoy@cn.ibm.com>
2020-11-30 22:21:25 +0800

    new feature: cli

---
+++ bin/lib/conf.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const yaml = require('yaml');
+
+
+
+module.exports = {
+    appId: "",
+    host: "",
+    port: "",
+    username: "",
+    password: ""
+}
---
+++ bin/lib/init.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const os = require('os');
+const del = require('del');
+const path = require('path');
+const yaml = require('yaml');
+const input = require('input');
+const {Base64} = require('js-base64');
+
+async function createConfig(){
+    const dp = path.join(os.homedir(), '.txrouter');
+    const fp = path.join(dp, 'txrouter.yaml');
+
+    await del(dp, {force: true});
+    const obj = {
+        apiVersion: 1,
+        description: "txrouter default configuration",
+        host: await input.text('host:'),
+        port: await input.text('port:'),
+        username: Base64.encode(await input.text('username:')),
+        password: Base64.encode(await input.text('password:'))
+    };
+    
+    fs.mkdirSync(dp);
+    fs.writeFileSync(fp, yaml.stringify(obj));
+
+    console.log('Done!');
+}
+
+module.exports = {
+    initConfig: async function(){
+        const fp = path.join(os.homedir(), '.txrouter')
+        if(fs.existsSync(fp)){
+            const answer = await input.text(`configuration already existed, do you want to replace it (${fp})? [y/N]:`);
+            if(['Y', 'y', 'YES', 'yes'].indexOf(answer) > -1){
+                createConfig();
+            }else if(['N', 'n', 'NO', 'no'].indexOf(answer) > -1){
+                console.log('Process terminated.');
+                process.exit(1);
+            }else{
+                console.log('Process terminated.');
+                process.exit(1);
+            }
+        }else{
+            createConfig();
+        }
+    }
+}
